abd070ec12afca0e54a733c201304d5685360afc
VaishaliSiri <Vaishali.Sirimalla@zeuslearning.com>
2025-06-11 19:08:19 +0530

    task3 done

---
+++ task3/script.js
@@ -0,0 +1,32 @@
+function array_cal(n, arr) {
+    let new_arr = [];
+    let carry = 0;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let val = ((n * arr[i]) + carry);
+        new_arr.unshift(val % 10);
+        // val %= 10;
+        carry = Math.floor(val / 10);
+
+
+    }
+    while (carry != 0) {
+        new_arr.unshift(carry % 10);
+        carry = Math.floor(carry / 10);
+    }
+    return new_arr;
+}
+let myArr = [1];
+function solve() {
+
+    for (let i = 1; i <= 1000; i++) {
+        myArr = array_cal(i, myArr);
+        // console.log(myArr)
+    }
+    const res = myArr.join('');
+    return res;
+}
+
+
+console.log(solve());
+
+
